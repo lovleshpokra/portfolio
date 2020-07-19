65a45e23135d0cfb6cf05929fc0d5573752f8ef6
Lovlesh Pokra <me@LovleshPokra.com>
2020-07-19 18:55:25 +0530

    Website Code

--- src/Wrapper/index.js
+++ src/Wrapper/index.js
@@ -1,13 +1,8 @@
 import React from "react";
-import bgImg from "./../assets/lovleshpokra.com_bg-image.jpg";
 
-function Wrapper({ children }) {
+function Wrapper() {
   return (
     <div className="wrapper">
-      <div className="platform">
-        <img src={bgImg} />
-      </div>
-      <div className="overlay">{children}</div>
     </div>
   );
 }
